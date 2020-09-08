dad23390ca2694c429b63c88c21c19275b38a948
Chris Jensen <cjj39@cam.ac.uk>
2020-09-08 10:41:55 +0100

    Update test

--- test/main.go
+++ test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	api "github.com/Cjen1/rc_op_go"
+	client_api "github.com/Cjen1/rc_op_go/client_api"
 	"log"
 	"sync"
 )
@@ -9,7 +10,10 @@ import (
 func do(client *api.Client, wg *sync.WaitGroup) {
 	defer wg.Done()
 	res := client.Write([]byte("test"), []byte("test1"))
-	if res.String() != "success" {
+	switch (res.Which()) {
+	case client_api.ClientResponse_Which_success :
+		log.Printf("Performed Write")
+	default:
 		log.Printf("Received %s instead of %s", res.String(), "success")
 		panic("Received incorrect response")
 	}
@@ -21,7 +25,7 @@ func main() {
 	cli := api.Create(101)
 	cli.AddConnection("127.0.0.1:5000")
 	var wg sync.WaitGroup
-	for i := 0; i < 1; i++ {
+	for i := 0; i < 100000; i++ {
 		wg.Add(1)
 		do(cli, &wg)
 	}
